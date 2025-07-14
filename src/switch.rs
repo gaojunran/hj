0cbc39bdb492a11f974cfc88f3064ade213990d4
gaojunran <nebula2021@126.com>
2025-07-14 22:29:29 +0800

    fix: use `edit` instead of `new` in `switch`

--- src/switch.rs
+++ src/switch.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 use duct::cmd;
 
 use crate::{check_git_installed, config::AppConfig, keepup::command_keepup};
@@ -11,7 +13,9 @@ pub(crate) fn command_switch(
     if config.switch_config.keepup || keepup {
         command_keepup(config, &vec![branch.clone()])?;
     }
-    cmd!("jj", "new", &branch).run()?;
+    // if there are multiple commits on top of the branch bookmark, switch to the latest one
+    let rev = format!("latest({branch}+)");
+    cmd!("jj", "edit", &rev).run()?;
     if git {
         check_git_installed()?;
         cmd!("git", "checkout", &branch).run()?;
