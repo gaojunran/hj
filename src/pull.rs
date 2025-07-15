41edb21fab1a5db0929fbd32f27616290b02b577
gaojunran <nebula2021@126.com>
2025-07-15 19:23:37 +0800

    feat: give steps in `hj pull`

--- src/pull.rs
+++ src/pull.rs
@@ -1,10 +1,16 @@
 use duct::cmd;
 
-use crate::{config::AppConfig, push::command_push, utils::warning};
+use crate::{
+    config::AppConfig,
+    push::command_push,
+    utils::{step, warning},
+};
 
 pub(crate) fn command_pull(config: &AppConfig, branch: Option<String>) -> anyhow::Result<()> {
+    step("Fetching changes from the remote...");
     cmd!("jj", "git", "fetch").run()?;
     if let Some(branch) = branch {
+        step(format!("Rebasing on `{branch}`...").as_str());
         cmd!("jj", "rebase", "-d", &branch).run()?;
     } else {
         warning("No branch specified in `hj pull`. You may need to rebase by yourself.");
